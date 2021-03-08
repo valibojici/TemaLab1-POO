86c0d8cc789c8bea4010dd6eccba49022f496c9b
Vali <valibojici@gmail.com>
2021-03-08 20:33:15 +0200

    change cost

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -16,7 +16,7 @@ int main()
 	a.set_brakePadRearCost(50);
 	a.set_brakeDiscCost(100);
 	a.set_tireCost(350);
-	a.set_wheelAlignmentCost(400);
+	a.set_wheelAlignmentCost(100);
 	a.set_oilCost(60);
 	a.set_cleaningProductsCost(100);
 	a.set_batteryCost(200);
